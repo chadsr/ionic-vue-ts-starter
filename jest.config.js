0543bc23164206424c1381ede0ed95b76ff33844
Ross <git@ross.ch>
2022-05-09 11:25:25 +0200

    codecov coverage reports

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest/presets/typescript-and-babel',
   transformIgnorePatterns: ['/node_modules/(?!@ionic/vue|@ionic/vue-router|@ionic/core|@stencil/core|ionicons)'],
+  collectCoverage: true,
+  coverageReporters: ['json', 'lcov'],
   // transform: {
   //   '^.+\.js$': 'babel-jest',
   //   '.\.(vue)$': 'vue-jest'
