f4aaf20cc3d806685c3e77d2a0a99c92bc618244
Ross <9055337+chadsr@users.noreply.github.com>
2022-05-09 13:23:02 +0200

    init commitizen + fix husk pre-commit (#22)

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
   env: {
     node: true,
   },
+  ignorePatterns: ['node_modules/**/*', 'ios/**/*', 'android/**/*', 'dist/**/*', '.yarn/**/*'],
   extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/typescript/recommended'],
   parserOptions: {
     ecmaVersion: 2020,
